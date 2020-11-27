d86ba567d73d3dd9ad0ad209cbb717bbc32f419a
Claire Tregunna <c.tregunna1980@gmail.com>
2020-11-27 20:35:16 +0000

    add array and get day function

--- app.js
+++ app.js
@@ -7,9 +7,15 @@ const planDiv = document.querySelector('.plan');
 const mealList = document.getElementById('meals');
 // const customer = prompt('What is your name?');
 const name = 'Chef Claire'; // Assign your name
-const starter = ['Brushetta', 'Soup', 'Prawn Cocktail'];
-const mainCourse = ['Steak and Chips', 'Tomato and Mozeralla Pasta', 'Fish Pie'];
-const pudding =  ['Jelly and Ice Cream', 'Eton Mess', 'Chocolate Fondant']; // Add in some meals...
+const meals = [
+    ['Brushetta', 'Soup', 'Prawn Cocktail', 'Pate'],
+    ['Steak and Chips', 'Tomato and Mozeralla Pasta', 'Fish Pie'],
+    ['Jelly and Ice Cream', 'Eton Mess', 'Chocolate Fondant'],
+    ['Juice', 'Cola', 'Water'],
+];
+// const starter = ['Brushetta', 'Soup', 'Prawn Cocktail'];
+// const mainCourse = ['Steak and Chips', 'Tomato and Mozeralla Pasta', 'Fish Pie'];
+// const pudding =  ['Jelly and Ice Cream', 'Eton Mess', 'Chocolate Fondant']; // Add in some meals...
 
 // HELPER FUNCTIONS
 
@@ -24,18 +30,25 @@ const getDay = () => {
     weekday[4] = "Thursday";
     weekday[5] = "Friday";
     weekday[6] = "Saturday";
-    console.log(weekday);
+    // console.log(weekday);
     return weekday[day.getDay()];
 
 };
 
+
 const getRandomWeeklyMeals = () => {
-    const randomStarter = Math.floor(Math.random() * starter.length);
-    const randomMain = Math.floor(Math.random() * mainCourse.length);
-    const randomPudding = Math.floor(Math.random() * pudding.length);
-    console.log(starter[randomStarter]);
-    console.log(mainCourse[randomMain]);
-    console.log(pudding[randomPudding]);
+    // const randomStarter = Math.floor(Math.random() * starter.length);
+    // const randomMain = Math.floor(Math.random() * mainCourse.length);
+    // const randomPudding = Math.floor(Math.random() * pudding.length);
+    // console.log(starter[randomStarter]);
+    // console.log(mainCourse[randomMain]);
+    // console.log(pudding[randomPudding]);
+    meals.forEach(meal => {
+        const randomDishes = Math.floor(Math.random() * meal.length);
+        // .toString().slice(0, 3)
+        console.log(randomDishes);
+    })
+
 };
 
 getRandomWeeklyMeals();
